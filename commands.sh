c65f0d5ce7404347ded322d265462b5fd39926e0
Tejas Mane <tejasmane485@gmail.com>
2023-03-11 10:40:36 +0530

    terraform commands added

---
+++ commands.sh
@@ -0,0 +1,55 @@
+# init
+# plan
+# apply
+# destroy
+# show ; -json - used to print output in json format
+
+# to check if the syntax is correct
+terraform validate
+
+# to format the code into a canonical format
+terraform fmt
+
+# to see a list of all providers in current configuration file
+terraform providers
+
+# to copy all the providers into another dir
+terraform providers mirror <dir-path>
+
+# to print all the outputs present in the current config dir
+terraform output
+# append name of a specific output for single result
+
+# to sync terraform with the real-world infrastructure
+# for example, manaul update in the infrastructure
+terraform refresh
+# this command will no modify any infrastructure resource but modifies the state file
+# this is also run by the command terraform plan and apply
+
+
+# terraform graph command is used to create a visual representation of the dependencies and a terraform config or an execution plan
+terraform graph
+# to make it in visual form use Graphviz and pass graph to it.
+
+# to install graphviz
+apt update
+apt install graphviz -y
+
+# to pass terraform graph output to graphviz
+terraform graph | dot -Tsvg > graph.svg
+
+# lifecycle rules: syntax
+lifecycle {
+	create_before_destroy = true
+}
+
+# ignore_chages rule
+# the ignore_chages argument accepts a list and it accepts any valid resource attributes
+lifecycle {
+	ignore_chagnes = [
+		tags
+	]
+}
+# here the change made to the tags of a server outside of terraform will be completely ignored
+# ingnore_changes = all 
+
